e844fff682f20f5acae310a3507ddcde6ce95438
jinyus <starboymc4@gmail.com>
2023-12-03 07:41:08 -0500

    Refactor buffered beacons to use ReadableBeacon

--- state_beacon/lib/src/beacons/buffered.dart
+++ state_beacon/lib/src/beacons/buffered.dart
@@ -1,6 +1,6 @@
 part of '../base_beacon.dart';
 
-class BufferedCountBeacon<T> extends WritableBeacon<List<T>> {
+class BufferedCountBeacon<T> extends ReadableBeacon<List<T>> {
   final int countThreshold;
   final List<T> _buffer = [];
   final _currentBuffer = WritableBeacon<List<T>>([]);
@@ -14,7 +14,7 @@ class BufferedCountBeacon<T> extends WritableBeacon<List<T>> {
     _currentBuffer.value = List.from(_buffer);
 
     if (_buffer.length == countThreshold) {
-      super.value = List.from(_buffer);
+      _setValue(List.from(_buffer));
       _buffer.clear();
     }
   }
@@ -26,7 +26,7 @@ class BufferedCountBeacon<T> extends WritableBeacon<List<T>> {
   }
 }
 
-class BufferedTimeBeacon<T> extends WritableBeacon<List<T>> {
+class BufferedTimeBeacon<T> extends ReadableBeacon<List<T>> {
   final Duration duration;
   final List<T> _buffer = [];
   final _currentBuffer = WritableBeacon<List<T>>([]);
@@ -45,7 +45,7 @@ class BufferedTimeBeacon<T> extends WritableBeacon<List<T>> {
   void _startTimerIfNeeded() {
     if (_timer == null || !_timer!.isActive) {
       _timer = Timer(duration, () {
-        super.value = List.from(_buffer);
+        _setValue(List.from(_buffer));
         _buffer.clear();
         _timer = null;
       });
