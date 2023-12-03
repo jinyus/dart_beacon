ae72979ad39c7f1528fedc724bc9b8906dad6af4
jinyus <starboymc4@gmail.com>
2023-12-03 10:33:16 -0500

    v0.3.2 - Expose ReadableBeacon and WritableBeacon

--- state_beacon/CHANGELOG.md
+++ state_beacon/CHANGELOG.md
@@ -1,3 +1,7 @@
+## 0.3.2
+
+-   Expose ReadableBeacon and WritableBeacon
+
 ## 0.3.1
 
 -   ThrottledBeacon: add method to change duration
--- state_beacon/lib/state_beacon.dart
+++ state_beacon/lib/state_beacon.dart
@@ -1,6 +1,6 @@
 library;
 
 export 'src/state_beacon.dart';
-export 'src/base_beacon.dart' show BaseBeacon;
+export 'src/base_beacon.dart' show BaseBeacon, ReadableBeacon, WritableBeacon;
 export 'src/async_value.dart';
 export 'src/extensions.dart';
