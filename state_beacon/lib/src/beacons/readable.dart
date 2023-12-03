8ca3ae84743eedfb2baf5ab303b928b1f478f183
jinyus <starboymc4@gmail.com>
2023-12-02 21:34:49 -0500

    super init from baseclass for readable

--- state_beacon/lib/src/beacons/readable.dart
+++ state_beacon/lib/src/beacons/readable.dart
@@ -1,10 +1,5 @@
 part of '../base_beacon.dart';
 
 class ReadableBeacon<T> extends BaseBeacon<T> {
-  ReadableBeacon([T? initialValue]) {
-    if (initialValue != null) {
-      _initialValue = initialValue;
-      _value = initialValue;
-    }
-  }
+  ReadableBeacon([super.initialValue]);
 }
