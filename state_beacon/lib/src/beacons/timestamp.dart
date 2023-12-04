ac567bb4ed3feb76bcde831c1d2864dcf532d7b8
jinyus <starboymc4@gmail.com>
2023-12-04 15:17:24 -0500

    Refactor TimestampBeacon constructor

--- state_beacon/lib/src/beacons/timestamp.dart
+++ state_beacon/lib/src/beacons/timestamp.dart
@@ -3,8 +3,10 @@ part of '../base_beacon.dart';
 typedef TimestampValue<T> = ({T value, DateTime timestamp});
 
 class TimestampBeacon<T> extends BaseBeacon<TimestampValue<T>> {
-  TimestampBeacon(T initialValue)
-      : super((value: initialValue, timestamp: DateTime.now()));
+  TimestampBeacon([T? initialValue])
+      : super(initialValue != null || null is T
+            ? (value: initialValue as T, timestamp: DateTime.now())
+            : null);
 
   void set(T newValue) {
     _setValue((value: newValue, timestamp: DateTime.now()));
