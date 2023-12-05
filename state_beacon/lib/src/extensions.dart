d97621a1bcf8a089105524cdc01add67fbb62937
jinyus <starboymc4@gmail.com>
2023-12-05 09:10:00 -0500

    add toValueNotifier, toStream

--- state_beacon/lib/src/extensions.dart
+++ state_beacon/lib/src/extensions.dart
@@ -1,3 +1,7 @@
+import 'dart:async';
+
+import 'package:flutter/foundation.dart';
+
 import 'base_beacon.dart';
 
 extension BoolUtils on WritableBeacon<bool> {
@@ -15,16 +19,37 @@ extension ListUtils<T> on List<T> {
 
 extension StreamUtils<T> on Stream<T> {
   /// Converts a stream to [StreamBeacon].
-  StreamBeacon<T> toBeacon() {
-    return StreamBeacon<T>(this);
+  StreamBeacon<T> toBeacon({bool cancelOnError = false}) {
+    return StreamBeacon<T>(this, cancelOnError: cancelOnError);
   }
 }
 
-// extension MapUtils<K, V> on Map<K, V> {
-//   /// Converts a map to [MapBeacon].
-//   MapBeacon<K, V> toBeacon() {
-//     return MapBeacon<K, V>(this);
-//   }
-// }
+extension WritableBeaconUtils<T> on WritableBeacon<T> {
+  /// Converts a [WritableBeacon] to [ValueNotifier]
+  ValueNotifier<T> toValueNotifier() {
+    final notifier = ValueNotifierBeacon(value);
 
+    final unsub = subscribe(notifier.set);
 
+    notifier.addListener(() => value = notifier.value);
+
+    notifier.addDisposeCallback(unsub);
+
+    return notifier;
+  }
+}
+
+extension ReadableBeaconUtils<T> on ReadableBeacon<T> {
+  /// Converts a [ReadableBeacon] to [Stream]
+  Stream<T> toStream() {
+    final controller = StreamController<T>();
+
+    controller.add(value);
+
+    final unsub = subscribe((v) => controller.add(v));
+
+    controller.onCancel = unsub;
+
+    return controller.stream;
+  }
+}
