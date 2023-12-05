6e467e25a54656c01e155d28e84231fa8974d099
jinyus <starboymc4@gmail.com>
2023-12-05 09:10:17 -0500

    Add ValueNotifierBeacon class

---
+++ state_beacon/lib/src/beacons/value_notifier.dart
@@ -0,0 +1,23 @@
+part of '../base_beacon.dart';
+
+class ValueNotifierBeacon<T> extends ValueNotifier<T> {
+  ValueNotifierBeacon(super.value);
+
+  final disposeListeners = <VoidCallback>[];
+
+  void addDisposeCallback(VoidCallback callback) {
+    disposeListeners.add(callback);
+  }
+
+  void set(T newValue) {
+    value = newValue;
+  }
+
+  @override
+  void dispose() {
+    for (var cb in disposeListeners) {
+      cb();
+    }
+    super.dispose();
+  }
+}
