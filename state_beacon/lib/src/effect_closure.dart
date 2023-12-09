640356df2cd83fd7763e6acb2a9059fedcf8a68e
jinyus <starboymc4@gmail.com>
2023-12-09 09:40:51 -0500

    Remove unnecessary hashCode and operator methods in EffectClosure class

--- state_beacon/lib/src/effect_closure.dart
+++ state_beacon/lib/src/effect_closure.dart
@@ -10,13 +10,5 @@ class EffectClosure {
   EffectClosure(this.run, {int? customID}) : id = customID ?? ++_globalEffectID;
 
   @override
-  int get hashCode => id.hashCode;
-
-  @override
-  bool operator ==(Object other) {
-    return other is EffectClosure && other.id == id;
-  }
-
-  @override
   String toString() => 'EffectClosure(id: $id)';
 }
