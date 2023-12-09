80ed27c3040b17188f22270a7595c883cee4242d
jinyus <starboymc4@gmail.com>
2023-12-08 22:44:57 -0500

    Add toRawBeacon ext method for Streams

--- state_beacon/lib/src/extensions/iterable.dart
+++ state_beacon/lib/src/extensions/iterable.dart
@@ -12,4 +12,20 @@ extension StreamUtils<T> on Stream<T> {
   StreamBeacon<T> toBeacon({bool cancelOnError = false}) {
     return StreamBeacon<T>(this, cancelOnError: cancelOnError);
   }
+
+  /// Converts a stream to [RawStreamBeacon].
+  RawStreamBeacon<T> toRawBeacon({
+    bool cancelOnError = false,
+    Function? onError,
+    Function? onDone,
+    T? initialValue,
+  }) {
+    return RawStreamBeacon<T>(
+      this,
+      cancelOnError: cancelOnError,
+      onError: onError,
+      onDone: onDone,
+      initialValue: initialValue,
+    );
+  }
 }
