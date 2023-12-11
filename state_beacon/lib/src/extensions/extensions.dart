20774a12309871a398119f990d33a58953e0a39d
jinyus <starboymc4@gmail.com>
2023-12-11 07:38:29 -0500

    remove flutter extensions

--- state_beacon/lib/src/extensions/extensions.dart
+++ state_beacon/lib/src/extensions/extensions.dart
@@ -3,8 +3,6 @@ import 'dart:async';
 import 'package:flutter/foundation.dart';
 import 'package:state_beacon/src/base_beacon.dart';
 
-export 'flutter.dart';
-
 part 'iterable.dart';
 part 'readable.dart';
 part 'writable.dart';
