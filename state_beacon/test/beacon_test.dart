c46405fffe861b2caf15693bd5048fcac12029b4
jinyus <starboymc4@gmail.com>
2023-12-14 14:56:31 -0500

    Refactor beacon_test.dart by removing unnecessary test cases

--- state_beacon/test/beacon_test.dart
+++ state_beacon/test/beacon_test.dart
@@ -144,69 +144,6 @@ void main() {
       expect(callCount, equals(0));
     });
 
-    test('should send 1 notification when doing batch updates', () {
-      final age = Beacon.writable<int>(10);
-      var callCount = 0;
-      age.subscribe((_) => callCount++);
-
-      Beacon.doBatchUpdate(() {
-        age.value = 15;
-        age.value = 16;
-        age.value = 20;
-        age.value = 23;
-      });
-
-      // There were 4 updates, but only 1 notification
-      expect(callCount, equals(1));
-    });
-
-    test('should not send notification when doing untracked access/updates',
-        () {
-      final age = Beacon.writable<int>(10);
-      var callCount = 0;
-      age.subscribe((_) => callCount++);
-
-      Beacon.createEffect(() {
-        age.value;
-        Beacon.untracked(() {
-          age.value = 15;
-        });
-      });
-
-      expect(callCount, equals(0));
-      expect(age.value, 15);
-
-      age.value = 20;
-      expect(callCount, equals(1));
-    });
-
-    test('should only notify once for nested batched updates', () {
-      final age = Beacon.writable<int>(10);
-      var callCount = 0;
-      age.subscribe((_) => callCount++);
-
-      Beacon.doBatchUpdate(() {
-        age.value = 15;
-        age.value = 16;
-        Beacon.doBatchUpdate(() {
-          age.value = 50;
-          age.value = 51;
-          Beacon.doBatchUpdate(() {
-            age.value = 100;
-            age.value = 200;
-          });
-          age.value = 52;
-        });
-        age.value = 20;
-      });
-
-      // There were 6 updates, but only 1 notification
-      expect(callCount, equals(1));
-
-      // The last value should be the one that was set last
-      expect(age.value, equals(20));
-    });
-
     test('should reset the value to initial state', () {
       var beacon = Beacon.writable<int>(10);
       beacon.value = 20;
@@ -595,7 +532,11 @@ void main() {
       var beacon1 = Beacon.writable<int>(10);
 
       try {
-        Beacon.createEffect(() => beacon1.value++);
+        Beacon.createEffect(() {
+          Beacon.doBatchUpdate(() {
+            beacon1.value++;
+          });
+        });
       } catch (e) {
         expect(e, isA<CircularDependencyException>());
       }
