05b48647e40bab68ec6cc97f053180f78368a83b
jinyus <starboymc4@gmail.com>
2023-12-15 15:35:19 -0500

    Refactor BeaconFamily caching logic

--- state_beacon/lib/src/beacons/family.dart
+++ state_beacon/lib/src/beacons/family.dart
@@ -2,19 +2,22 @@ import 'package:state_beacon/src/base_beacon.dart';
 
 class BeaconFamily<Arg, BeaconType extends BaseBeacon> {
   final bool shouldCache;
-  final Map<Arg, BeaconType> _cache = {};
+  late final Map<Arg, BeaconType> _cache;
   final BeaconType Function(Arg) _create;
 
-  BeaconFamily(this._create, {this.shouldCache = false});
+  BeaconFamily(this._create, {this.shouldCache = false}) {
+    if (shouldCache) {
+      _cache = {};
+    }
+  }
 
   /// Retrieves a `Beacon` based on the given argument.
   /// If caching is enabled and a beacon for the provided argument exists in the cache, it is returned.
   /// Otherwise, a new beacon is created using the `create` function.
   BeaconType call(Arg arg) {
-    if (shouldCache) {
-      return _cache[arg] ??= _create(arg);
-    }
-    return _create(arg);
+    if (!shouldCache) return _create(arg);
+
+    return _cache[arg] ??= _create(arg);
   }
 
   /// Clears the cache of beacon if caching is enabled.
