e792899ec2edd4f7ff8b6d32421835d27c94f5ed
jinyus <starboymc4@gmail.com>
2023-12-18 22:33:37 -0500

    Add test for creating future beacon

--- state_beacon/test/family_test.dart
+++ state_beacon/test/family_test.dart
@@ -1,12 +1,56 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:state_beacon/state_beacon.dart';
 
+import 'flutter_test.dart';
+
 void main() {
   test('should create a new beacon', () {
     var family = Beacon.family((int arg) => Beacon.writable(arg.toString()));
     var beacon = family(1);
 
     expect(beacon.value, '1');
+  });
+
+  test('should create a new future beacon', () async {
+    var counter = Beacon.writable(10);
+
+    var counterMirror = Beacon.derivedFuture(() async {
+      var count = counter.value;
+      await Future.delayed(k10ms);
+      return count;
+    });
+
+    var family = Beacon.family((int arg) => Beacon.derivedFuture(() async {
+          var count = await counterMirror.toFuture();
+          await Future.delayed(k10ms);
+          return (count * arg).toString();
+        }));
+
+    var doubled = family(2);
+
+    expect(doubled.value, AsyncLoading());
+
+    await Future.delayed(k10ms * 2.1);
+
+    expect(doubled.value.unwrapValue(), '20');
+
+    var tripled = family(3);
+
+    expect(tripled.value, AsyncLoading());
+
+    await Future.delayed(k10ms * 2.1);
+
+    expect(tripled.value.unwrapValue(), '30');
+
+    counter.increment();
+
+    expect(doubled.value, AsyncLoading());
+    expect(tripled.value, AsyncLoading());
+
+    await Future.delayed(k10ms * 4);
+
+    expect(doubled.value.unwrapValue(), '22');
+    expect(tripled.value.unwrapValue(), '33');
   });
 
   // Test for caching behavior
