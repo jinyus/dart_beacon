a729cde8212a4c4df17b002f4022c840df982149
jinyus <starboymc4@gmail.com>
2024-01-03 11:33:28 -0500

    Add WritableAsyncBeacon.tryCatch extension for handling asynchronous values

--- state_beacon/lib/src/extensions/extensions.dart
+++ state_beacon/lib/src/extensions/extensions.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:flutter/foundation.dart';
 import 'package:state_beacon/src/base_beacon.dart';
+import 'package:state_beacon/state_beacon.dart';
 
 part 'iterable.dart';
 part 'readable.dart';
