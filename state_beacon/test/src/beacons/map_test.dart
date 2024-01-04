74ea8c238c3cc0305b93294109ac4ff6858f8dd4
jinyus <starboymc4@gmail.com>
2024-01-04 15:17:04 -0500

    add tests for setbeacon and mapbeacon

---
+++ state_beacon/test/src/beacons/map_test.dart
@@ -0,0 +1,66 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:state_beacon/state_beacon.dart';
+
+void main() {
+  test('should set previous and initial values', () {
+    var beacon = Beacon.hashMap<String, int>({});
+    beacon.value = {'a': 1, 'b': 2};
+    expect(beacon.previousValue, equals(<String, int>{}));
+    beacon.value = {'c': 3, 'd': 4};
+    expect(beacon.previousValue, equals({'a': 1, 'b': 2}));
+    expect(beacon.initialValue, <String, int>{});
+  });
+
+  test('should notify listeners when map is modified', () {
+    var data = Beacon.hashMap<String, int>({});
+
+    var called = 0;
+
+    data.subscribe((_) => called++);
+
+    data['a'] = 1;
+
+    expect(called, 1);
+    expect(data.value, equals({'a': 1}));
+
+    data.addAll({'b': 2, 'c': 3});
+
+    expect(called, 2);
+    expect(data.value, equals({'a': 1, 'b': 2, 'c': 3}));
+
+    data.remove('b');
+
+    expect(called, 3);
+    expect(data.value, equals({'a': 1, 'c': 3}));
+
+    data.update('a', (value) => value! + 2);
+
+    expect(called, 4);
+    expect(data.value, equals({'a': 3, 'c': 3}));
+
+    data.clear();
+
+    expect(called, 5);
+    expect(data.value, equals(<String, int>{}));
+
+    data.addAll({'d': 4, 'e': 5});
+
+    expect(called, 6);
+    expect(data.value, equals({'d': 4, 'e': 5}));
+
+    data.removeWhere((key, value) => key.startsWith('d'));
+
+    expect(called, 7);
+    expect(data.value, equals({'e': 5}));
+
+    data.putIfAbsent('a', () => 1);
+
+    expect(called, 8);
+    expect(data.value, equals({'e': 5, 'a': 1}));
+
+    data.updateAll((key, value) => value + 1);
+
+    expect(called, 9);
+    expect(data.value, equals({'e': 6, 'a': 2}));
+  });
+}
