a21bc7169d89b717e395a3a3ce97de357882db64
jinyus <starboymc4@gmail.com>
2024-01-09 15:29:49 -0500

    format and minor refactor

--- state_beacon/lib/state_beacon.dart
+++ state_beacon/lib/state_beacon.dart
@@ -1,23 +1,24 @@
+/// reactive primitive and statemanagement for flutter
 library;
 
-export 'src/state_beacon.dart';
+export 'src/async_value.dart';
 export 'src/base_beacon.dart'
     show
         BaseBeacon,
-        ReadableBeacon,
-        WritableBeacon,
-        ListBeacon,
-        StreamBeacon,
         BufferedCountBeacon,
         BufferedTimeBeacon,
-        FutureBeacon,
-        FilteredBeacon,
         DebouncedBeacon,
-        TimestampBeacon,
-        ThrottledBeacon,
-        UndoRedoBeacon,
         DerivedBeacon,
-        DerivedFutureBeacon;
-export 'src/async_value.dart';
+        DerivedFutureBeacon,
+        FilteredBeacon,
+        FutureBeacon,
+        ListBeacon,
+        ReadableBeacon,
+        StreamBeacon,
+        ThrottledBeacon,
+        TimestampBeacon,
+        UndoRedoBeacon,
+        WritableBeacon;
 export 'src/extensions/extensions.dart';
 export 'src/observer.dart';
+export 'src/state_beacon.dart';
