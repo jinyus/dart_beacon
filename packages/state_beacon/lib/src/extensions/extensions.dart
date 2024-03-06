4dc4f98a5f78a4f24db313cfa0d65ba82a53dde3
jinyus <starboymc4@gmail.com>
2024-03-06 11:10:06 -0500

    add convenience extensions to ScopeRefs containing Beacons or BeaconControllers

--- packages/state_beacon/lib/src/extensions/extensions.dart
+++ packages/state_beacon/lib/src/extensions/extensions.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/widgets.dart';
+import 'package:lite_ref/lite_ref.dart';
 import 'package:state_beacon/src/value_notifier_beacon.dart';
 import 'package:state_beacon_core/state_beacon_core.dart';
 
 part 'readable.dart';
 part 'watch_observe.dart';
 part 'writable.dart';
+part 'scoped_ref.dart';
 
 final Map<int, ValueNotifierBeacon<dynamic>> _vnCache = {};
 
