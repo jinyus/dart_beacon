5bf7ff5d104f1b87050c0cf59503c1e0a8e657b4
jinyus <30532952+jinyus@users.noreply.github.com>
2024-04-03 17:02:10 -0500

    Add ValueNotifier.toBeacon() and TextEditingBeacon() (#106)
    * Add ValueNotifier.toBeacon()
    
    * Refactor safeWrite method in ValueNotifierUtils
    
    * add TextEditingBeacon
    
    * fix doc comments
    
    Co-authored-by: yehorh <yehorh@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: yehorh <yehorh@users.noreply.github.com>

--- packages/state_beacon/lib/state_beacon.dart
+++ packages/state_beacon/lib/state_beacon.dart
@@ -3,9 +3,7 @@ library;
 
 export 'package:lite_ref/lite_ref.dart';
 export 'package:state_beacon_core/state_beacon_core.dart' hide BeaconScheduler;
-
 export 'src/controller/controller.dart';
-
 export 'src/extensions/extensions.dart' hide hasNotifier;
-
+export 'src/notifier/text_editing_controller.dart';
 export 'src/scheduler.dart';
