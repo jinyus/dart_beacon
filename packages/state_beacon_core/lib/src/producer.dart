06819536d9ca14c91e8fc23a6c3473bac176dc81
jinyus <starboymc4@gmail.com>
2024-04-08 11:39:42 -0500

    update doc comment

--- packages/state_beacon_core/lib/src/producer.dart
+++ packages/state_beacon_core/lib/src/producer.dart
@@ -190,8 +190,8 @@ abstract class Producer<T> implements Disposable {
   /// with the current value of the beacon.
   ///
   /// If [synchronous] is true, the callback will be ran synchronously.
-  /// This also means automatic batching of updates will be disabled
-  /// and any Scheduler will be ignored.
+  /// This also means that the scheduler will be ignored and automatic
+  /// batching of updates will be disabled.
   VoidCallback subscribe(
     void Function(T) callback, {
     bool startNow = true,
