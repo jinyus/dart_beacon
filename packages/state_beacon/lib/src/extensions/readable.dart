8a3743c310756492943aaa8b111e0ad8fbeefa47
jinyus <starboymc4@gmail.com>
2024-05-30 08:19:42 -0500

    remove duplicate files state_beacon

--- packages/state_beacon/lib/src/extensions/readable.dart
+++
@@ -1,9 +0,0 @@
-part of 'extensions.dart';
-
-// ignore: public_member_api_docs
-extension ReadableBeaconFlutterUtils<T> on ReadableBeacon<T> {
-  /// Converts this to a [ValueListenable]
-  ValueListenable<T> toListenable() {
-    return _toValueNotifier(this);
-  }
-}
--- packages/state_beacon/lib/state_beacon.dart
+++ packages/state_beacon/lib/state_beacon.dart
@@ -2,8 +2,5 @@
 library;
 
 export 'package:lite_ref/lite_ref.dart';
-export 'package:state_beacon_core/state_beacon_core.dart' hide BeaconScheduler;
-export 'src/controller/controller.dart';
-export 'src/extensions/extensions.dart' hide hasNotifier;
-export 'src/notifier/text_editing_controller.dart';
-export 'src/scheduler.dart';
+export 'package:state_beacon_flutter/state_beacon_flutter.dart';
+export 'src/extensions/scoped_ref.dart';
