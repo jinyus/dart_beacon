78106fe2b89ce4fce703c6b5d2a34aca157832f3
jinyus <starboymc4@gmail.com>
2025-11-13 20:38:49 -0500

    update examples deps

--- examples/shopping_cart/lib/src/app.dart
+++ examples/shopping_cart/lib/src/app.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:shopping_cart/src/cart/view.dart';
 import 'package:shopping_cart/src/catalog/view.dart';
+import 'package:shopping_cart/src/localization/app_localizations.dart';
 
 /// The Widget that configures your application.
 class MyApp extends StatelessWidget {
--- examples/vgv_best_practices/lib/l10n/l10n.dart
+++ examples/vgv_best_practices/lib/l10n/l10n.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/widgets.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-
-export 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:vgv_best_practices/l10n/arb/app_localizations.dart';
+export 'package:vgv_best_practices/l10n/arb/app_localizations.dart';
 
 extension AppLocalizationsX on BuildContext {
   AppLocalizations get l10n => AppLocalizations.of(this);
