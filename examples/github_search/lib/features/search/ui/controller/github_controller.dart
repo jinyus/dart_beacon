81c407b3bf6630f5cb32d86e48bb594e42cbb66e
jinyus <starboymc4@gmail.com>
2025-11-30 08:36:44 -0500

    update gothub search example

--- examples/github_search/lib/features/search/ui/controller/github_controller.dart
+++ examples/github_search/lib/features/search/ui/controller/github_controller.dart
@@ -1,35 +1,26 @@
 import 'package:github_search/core/constants.dart';
+import 'package:github_search/features/search/models/search_result.dart';
 import 'package:github_search/features/search/repo/github_search.dart';
 import 'package:state_beacon/state_beacon.dart';
 
-class GithubController with BeaconController {
+class GithubController extends BeaconController {
   GithubController(this._repo) {
-    _startSearchingOnValidQuery();
+    searchTermDebounced.next().then((_) => _results.start());
   }
 
   final GithubSearchRepository _repo;
 
-  late final _searchTerm = B.debounced('', duration: k100ms * 5);
+  late final searchTerm = TextEditingBeacon();
 
-  late final results = B.future(
-    () => _repo.search(_searchTerm.value),
+  late final searchTermDebounced =
+      searchTerm.filter((_, n) => n.text.isNotEmpty).debounce(k100ms * 5);
+
+  late final _results = B.future(
+    () => _repo.search(searchTermDebounced.value.text),
     manualStart: true,
   );
 
-  void onTextChanged(String text) => _searchTerm.set(text);
+  ReadableBeacon<AsyncValue<SearchResult>> get results => _results;
 
-  void retry() => results.reset();
-
-  // this listens to the search term beacon and starts the future beacon
-  // if the search term is valid, otherwise, it will set the future beacon
-  // to idle
-  void _startSearchingOnValidQuery() {
-    _searchTerm.subscribe((val) {
-      if (val.trim().isEmpty) {
-        results.idle();
-      } else if (results.isIdle) {
-        results.start();
-      }
-    });
-  }
+  void retry() => _results.reset();
 }
--- examples/github_search/lib/features/search/ui/search_bar.dart
+++ examples/github_search/lib/features/search/ui/search_bar.dart
@@ -1,41 +1,18 @@
 part of 'search_page.dart';
 
-class _SearchBar extends StatefulWidget {
-  @override
-  State<_SearchBar> createState() => _SearchBarState();
-}
-
-class _SearchBarState extends State<_SearchBar> {
-  final _textController = TextEditingController();
-
-  @override
-  void initState() {
-    super.initState();
-    final controller = searchControllerRef.read(context);
-    _textController.addListener(() {
-      controller.onTextChanged(_textController.text);
-    });
-  }
-
-  @override
-  void dispose() {
-    _textController.dispose();
-    super.dispose();
-  }
-
+class _SearchBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final controller = searchControllerRef.of(context);
+    final controller = searchControllerRef.of(context).searchTerm.controller;
     return Padding(
       padding: const EdgeInsets.all(8),
       child: TextField(
-        controller: _textController,
+        controller: controller,
         autocorrect: false,
-        onChanged: controller.onTextChanged,
         decoration: InputDecoration(
           prefixIcon: const Icon(Icons.search),
           suffixIcon: GestureDetector(
-            onTap: _textController.clear,
+            onTap: controller.clear,
             child: const Icon(Icons.clear),
           ),
           border: const OutlineInputBorder(),
