3461cd427fbb78a50942f7b8f75f45370f7ec7ff
jinyus <starboymc4@gmail.com>
2025-12-02 08:26:05 -0500

    update shopping cart example to use updateWith

--- examples/shopping_cart/lib/src/cart/controller.dart
+++ examples/shopping_cart/lib/src/cart/controller.dart
@@ -7,30 +7,29 @@ import 'package:state_beacon/state_beacon.dart';
 import 'service.dart';
 
 class CartController extends BeaconController {
-  CartController(this._cartService) {
-    _cart.tryCatch(() => _cartService.loadProducts());
-  }
+  CartController(this._cartService);
 
   final CartService _cartService;
-
-  late final _cart = B.writable<AsyncValue<Cart>>(AsyncIdle());
 
   // this is used to grey out the Add button of an item while it is being added to the cart
   // so the user cannot add it twice
   late final addingItems = B.family((int id) => B.writable(false));
   late final removingItems = B.family((int id) => B.writable(false));
 
+  late final _cart = B.future(_cartService.loadProducts);
+
+  // expose it as immutable so it can only be modified by the controller
   ReadableBeacon<AsyncValue<Cart>> get cart => _cart;
 
   Future<void> addItem(Product item) async {
     addingItems(item.id).value = true;
-    await _cart.tryCatch(() => _cartService.add(item));
+    await _cart.updateWith(() => _cartService.add(item));
     addingItems(item.id).value = false;
   }
 
   Future<void> removeItem(Product item) async {
     removingItems(item.id).value = true;
-    await _cart.tryCatch(() => _cartService.remove(item));
+    await _cart.updateWith(() => _cartService.remove(item));
     removingItems(item.id).value = false;
   }
 }
