0750a973cd8c393d6b0acdcbc748f022bb501ebb
jinyus <starboymc4@gmail.com>
2025-12-16 08:08:21 -0500

    examples: fix counter test

--- examples/counter/test/controller_test.dart
+++ examples/counter/test/controller_test.dart
@@ -3,26 +3,34 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:state_beacon/state_beacon.dart';
 
 void main() {
-  test('emits [1] when incremented', () {
+  test('emits [1] when incremented', () async {
     final controller = Controller();
 
     expect(controller.count.value, 0);
 
     final emitted = controller.count.buffer(2);
 
+    BeaconScheduler.flush();
+
     controller.increment();
+
+    BeaconScheduler.flush();
 
     expect(emitted.value, [0, 1]);
   });
 
-  test('emits [-1] when decremented', () {
+  test('emits [-1] when decremented', () async {
     final controller = Controller();
 
     expect(controller.count.value, 0);
 
     final emitted = controller.count.buffer(2);
 
+    BeaconScheduler.flush();
+
     controller.decrement();
+
+    BeaconScheduler.flush();
 
     expect(emitted.value, [0, -1]);
   });
